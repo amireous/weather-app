e89d7fa6c1de9985fa7600bdb337ff8c7877cfc7
Amir SF <82820601+amireous@users.noreply.github.com>
2021-08-13 00:35:13 +0430

    add all-styles-and-logic

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 const selectOptions = document.getElementById("options");
 const weatherSvg = document.getElementById("svg-img");
-
 const loader = document.querySelector(".loading");
+
+let isLoading = false;
 
 selectOptions.addEventListener("change", function () {
   getCityData(document.querySelector(`option[value="${this.value}"]`).id);
@@ -11,7 +12,7 @@ selectOptions.addEventListener("change", function () {
 
 const renderHTML = function (data) {
   const weatherDetail = document.getElementById("weather-detail");
-  const cityName = document.getElementById("city");
+  const cityName = document.getElementById("city-name");
   const weatherTemp = document.getElementById("temp-number");
 
   cityName.textContent = data.name;
@@ -19,18 +20,25 @@ const renderHTML = function (data) {
   weatherTemp.textContent = (data.main.temp - 273).toFixed(0);
   weatherSvg.style.opacity = 1;
 
-  document.querySelector(".fa").style.display = "block";
+  document.querySelector(".fa-map-marker-alt").style.display = "block";
   swichWeather(data.weather[0].main.toLowerCase());
-
-  console.log(data.weather[0].main.toLowerCase());
 };
 
 const getCityData = function (city) {
+  isLoading = true;
+  if (isLoading) loader.style.opacity = 1;
+
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3778a864a1c7314c3191fc8a6ef44a31`
   )
     .then((request) => request.json())
-    .then((data) => renderHTML(data));
+    .then((data) => {
+      renderHTML(data);
+
+      isLoading = false;
+      if (isLoading === false) loader.style.opacity = 0;
+    })
+    .catch((err) => alert(`Something went wrong ${err.message} ❗`));
 };
 
 const swichWeather = function (weatherStatus) {
